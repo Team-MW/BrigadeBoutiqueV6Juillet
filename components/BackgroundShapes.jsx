b7d3e3ab47426932ecbd9f900093c61e4e54fe7c
sofianelamine772-design <mwcrea.agency@gmail.com>
2026-07-06 22:39:16 +0200

    d

--- components/BackgroundShapes.jsx
+++ components/BackgroundShapes.jsx
@@ -7,8 +7,8 @@ export default function BackgroundShapes() {
       <div className={`${styles.shape} ${styles.shape2}`}></div>
       <div className={`${styles.shape} ${styles.shape3}`}></div>
       <div className={`${styles.shape} ${styles.shape4}`}></div>
-      
-      {/* Overlay pour donner de la texture (optionnel, effet noise) */}
+
+      {/* Overlaydede pour donner de la texture (optionnel, effet noise) */}
       <div className={styles.noiseOverlay}></div>
     </div>
   );
